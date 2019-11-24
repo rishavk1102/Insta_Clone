1a029a663b7337ed1645d6f5df4081da71ff22af
Rishav <rishavk1102@gmail.com>
2019-11-24 12:53:41 +0530

    1. Search Feature 2. Follow Feature 3. Unfollow Feature

---
+++ app/src/main/java/com/rishav/instaclone/Adapter/UserAdapter.java
@@ -0,0 +1,140 @@
+package com.rishav.instaclone.Adapter;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rishav.instaclone.Fragment.ProfileFragment;
+import com.rishav.instaclone.Model.User;
+import com.rishav.instaclone.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
+
+    private Context mContext;
+    private List<User> mUsers;
+
+    private FirebaseUser firebaseUser;
+
+    public UserAdapter(Context mContext, List<User> mUsers) {
+        this.mContext = mContext;
+        this.mUsers = mUsers;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item , parent , false);
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        final User user = mUsers.get(position);
+        holder.btn_follow.setVisibility(View.VISIBLE);
+
+        holder.username.setText(user.getUsername());
+        holder.fullname.setText(user.getFullname());
+        Picasso.get().load(user.getImageurl()).placeholder(R.drawable.default_avtar).into(holder.image_profile);
+        isFollowed(user.getId() , holder.btn_follow);
+
+        if (user.getId().equals(firebaseUser.getUid())){
+            holder.btn_follow.setVisibility(View.GONE);
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = mContext.getSharedPreferences("PREFS" , Context.MODE_PRIVATE).edit();
+                editor.putString("profileid" , user.getId());
+                editor.apply();
+
+                ((FragmentActivity)mContext).getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , new ProfileFragment()).commit();
+            }
+        });
+
+        holder.btn_follow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (holder.btn_follow.getText().toString().equals("follow")){
+                    FirebaseDatabase.getInstance().getReference().child("Follow").child(firebaseUser.getUid())
+                            .child("following").child(user.getId()).setValue(true);
+
+                    FirebaseDatabase.getInstance().getReference().child("Follow").child(user.getId())
+                            .child("followers").child(firebaseUser.getUid()).setValue(true);
+                } else {
+                    FirebaseDatabase.getInstance().getReference().child("Follow").child(firebaseUser.getUid())
+                            .child("following").child(user.getId()).removeValue();
+
+                    FirebaseDatabase.getInstance().getReference().child("Follow").child(user.getId())
+                            .child("followers").child(firebaseUser.getUid()).removeValue();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView username;
+        public TextView fullname;
+        public CircleImageView image_profile;
+        public Button btn_follow;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            fullname = itemView.findViewById(R.id.fullname);
+            image_profile = itemView.findViewById(R.id.image_profile);
+            btn_follow = itemView.findViewById(R.id.btn_follow);
+        }
+    }
+
+    private void isFollowed (final String userid , final Button button) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Follow").child(firebaseUser.getUid()).child("following");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child(userid).exists()){
+                    button.setText("following");
+                } else {
+                    button.setText("follow");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/rishav/instaclone/Fragment/SearchFragment.java
+++ app/src/main/java/com/rishav/instaclone/Fragment/SearchFragment.java
@@ -2,20 +2,123 @@ package com.rishav.instaclone.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.rishav.instaclone.Adapter.UserAdapter;
+import com.rishav.instaclone.Model.User;
 import com.rishav.instaclone.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private UserAdapter userAdapter;
+    private List<User> mUsers;
+
+    private EditText search_bar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        search_bar = view.findViewById(R.id.search_bar);
+
+        mUsers = new ArrayList<>();
+        userAdapter = new UserAdapter(getContext() , mUsers);
+        recyclerView.setAdapter(userAdapter);
+
+        readUsers();
+        search_bar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                searchUser(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        return view;
+    }
+
+    private void searchUser (String s) {
+        Query query = FirebaseDatabase.getInstance().getReference().child("Users").orderByChild("username").startAt(s)
+                .endAt(s + "\uf8ff");
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mUsers.clear();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    User user = snapshot.getValue(User.class);
+                    mUsers.add(user);
+                }
+
+                userAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void readUsers () {
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (TextUtils.isEmpty(search_bar.getText().toString())){
+                    mUsers.clear();
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                        User user = snapshot.getValue(User.class);
+                        mUsers.add(user);
+                    }
+
+                    userAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
