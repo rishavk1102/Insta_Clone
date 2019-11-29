7477d52580d73f0827188e78550cb59f28ecdec3
Rishav <rishavk1102@gmail.com>
2019-11-29 21:45:10 +0530

    Viewing posts in User Profile

---
+++ app/src/main/java/com/rishav/instaclone/Adapter/MyFotoAdapter.java
@@ -0,0 +1,61 @@
+package com.rishav.instaclone.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.rishav.instaclone.Model.Post;
+import com.rishav.instaclone.R;
+
+import java.util.List;
+
+public class MyFotoAdapter extends RecyclerView.Adapter<MyFotoAdapter.ViewHolder>{
+
+    private Context context;
+    private List<Post> mPosts;
+
+    public MyFotoAdapter(Context context, List<Post> mPosts) {
+        this.context = context;
+        this.mPosts = mPosts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.fotos_item , parent , false);
+        return new MyFotoAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        Post post = mPosts.get(position);
+        Glide.with(context).load(post.getPostimage()).into(holder.post_image);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPosts.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView post_image;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            post_image = itemView.findViewById(R.id.post_image);
+        }
+    }
+
+}
+
+
--- app/src/main/java/com/rishav/instaclone/Fragment/ProfileFragment.java
+++ app/src/main/java/com/rishav/instaclone/Fragment/ProfileFragment.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,10 +25,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.rishav.instaclone.Adapter.MyFotoAdapter;
 import com.rishav.instaclone.Model.Post;
 import com.rishav.instaclone.Model.User;
 import com.rishav.instaclone.R;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ProfileFragment extends Fragment {
 
@@ -40,6 +48,10 @@ public class ProfileFragment extends Fragment {
     private ImageButton my_fotos;
     private ImageButton saved_fotos;
     private Button editprofile;
+
+    private RecyclerView recyclerView;
+    private MyFotoAdapter myFotoAdapter;
+    List<Post> postList;
 
     private FirebaseUser firebaseUser;
     String profileid;
@@ -65,9 +77,18 @@ public class ProfileFragment extends Fragment {
         saved_fotos = view.findViewById(R.id.saved_fotos);
         editprofile = view.findViewById(R.id.edit_profile);
 
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new GridLayoutManager(getContext() , 3);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        postList = new ArrayList<>();
+        myFotoAdapter = new MyFotoAdapter(getContext() , postList);
+        recyclerView.setAdapter(myFotoAdapter);
+
         userInfo();
         getFollowers();
         getNrPosts();
+        myFotos();
 
         if (profileid.equals(firebaseUser.getUid())){
             editprofile.setText("Edit Profile");
@@ -198,6 +219,30 @@ public class ProfileFragment extends Fragment {
 
                 posts.setText("" + i);
 
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void myFotos() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                postList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Post post = snapshot.getValue(Post.class);
+                    if (post.getPublisher().equals(profileid)){
+                        postList.add(post);
+                    }
+                }
+
+                Collections.reverse(postList);
+                myFotoAdapter.notifyDataSetChanged();
             }
 
             @Override
